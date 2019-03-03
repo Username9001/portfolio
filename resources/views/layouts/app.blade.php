eb10c6214d91569dd88cb2a16ed432e7f66f95d6
Username9001 <erik.wubbels@gmail.com>
2019-03-03 20:44:41 +0100

    service worker test2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -130,11 +130,7 @@ ob_start("ob_gzhandler");
         console.log('CLIENT: service worker registration in progress.');
         navigator.serviceWorker.register('/service-worker.js').then(function() {
             console.log('CLIENT: service worker registration complete.');
-        }, function() {
-            console.log('CLIENT: service worker registration failure.');
         });
-        } else {
-        console.log('CLIENT: service worker is not supported.');
-        }
+        } 
     </script>
 </html>
