0a5b9d01f95a70cc2c3b59c0ab3863b8c787a501
Username9001 <erik.wubbels@gmail.com>
2019-03-03 20:54:44 +0100

    service3

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,10 +4,10 @@ var cacheName = 'v1';
 // Default files to always cache
 var cacheFiles = [
 	'/',
-	'/index.html',
-	'/js/app.js',
-	'/css/app.css',
-	'/css/aos.css',
+	'index.html',
+	'js/app.js',
+	'css/app.css',
+	'css/aos.css',
 	'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic'
 ]
 
