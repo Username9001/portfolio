a1b6cd3cc6680c11c007d857dec90a89e2d28232
Username9001 <erik.wubbels@gmail.com>
2019-03-03 23:53:14 +0100

    service worker offline test4

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,65 +1,26 @@
-var cacheName = 'v1'; 
-
-// Default files to always cache
-var cacheFiles = [
-	'/',
-	'/index.html',
-	'/js/app.js',
-	'/css/app.css',
-	'/css/aos.css'
-]
-
-
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Installed');
-});
-
-
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activated');
-});
-
-self.addEventListener('install', function(event) {
+let CACHE_NAME = 'sw-v1'
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(cacheFiles);
-    })
-  );
+    .then(cache => cache.addAll('./404.html'))
+  )
+})
+self.addEventListener('fetch', (event) => {
+  if (event.request.method === 'GET') {
+    event.respondWith(
+      caches.match(event.request)
+      .then((cached) => {
+        var networked = fetch(event.request)
+          .then((response) => {
+            let cacheCopy = response.clone()
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, cacheCopy))
+            return response;
+          })
+          .catch(() => caches.match(offlinePage));
+        return cached || networked;
+      })
+    )
+  }
+  return;
 });
-
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-	  caches.match(event.request)
-	  .then(function(response) {
-		return response || fetchAndCache(event.request);
-	  })
-	);
-  });
-  
-  let CACHE_NAME = 'sw-v1'
-  self.addEventListener('install', (event) => {
-	event.waitUntil(
-	  caches.open(CACHE_NAME)
-	  .then(cache => cache.addAll('./404.html'))
-	)
-  })
-  self.addEventListener('fetch', (event) => {
-	if (event.request.method === 'GET') {
-	  event.respondWith(
-		caches.match(event.request)
-		.then((cached) => {
-		  var networked = fetch(event.request)
-			.then((response) => {
-			  let cacheCopy = response.clone()
-			  caches.open(CACHE_NAME)
-				.then(cache => cache.put(event.request, cacheCopy))
-			  return response;
-			})
-			.catch(() => caches.match(offlinePage));
-		  return cached || networked;
-		})
-	  )
-	}
-	return;
-  });
