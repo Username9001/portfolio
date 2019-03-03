0781fce9e901eb2b326425842f1f90eafb4dad85
Username9001 <erik.wubbels@gmail.com>
2019-03-03 20:51:32 +0100

    service

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,8 +30,3 @@
 //     el: '#app'
 // });
 
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('sw.js')
-        .then(function() { console.log("Service Worker Registered"); });
-    }
