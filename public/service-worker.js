1f626d8f9221f32f72a54ad6422f16dee14eb180
Username9001 <erik.wubbels@gmail.com>
2019-03-04 00:23:40 +0100

    service worker offline test17

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'index.html',
+//   'index.html',
   '/', // Alias for index.html
   '/css/app.css',
   '/css/aos.css',
