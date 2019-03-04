cea4fec3ec0a5b9e8564c8263cf42bc36d4d8c28
Username9001 <erik.wubbels@gmail.com>
2019-03-04 02:22:03 +0100

    lazy weg

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -66,24 +66,24 @@
         <div class="web-clickb4it">
             <a href="/clickb4it" aria-label="clickbaitwarez webshop">
                 <div class="wireframe-container" data-aos="fade-right" data-aos-duration="1400" data-aos-easing="ease-in-out" data-aos-anchor-placement="top-bottom" data-aos-delay="300">
-                    <img src="/storage/blank.jpg" data-src="/storage/laptop.png" alt="" class="lazy test-image device-image img1">
-                    <img src="/storage/blank.jpg" data-src="/storage/clickb4it_fs.jpg" alt="" class="lazy test-image img2">
+                    <img src="/storage/laptop.png" alt="" class="lazy test-image device-image img1">
+                    <img src="/storage/clickb4it_fs.jpg" alt="" class="lazy test-image img2">
                 </div>
                 <div class="wireframe-container2" data-aos="fade-left" data-aos-duration="1400" data-aos-easing="ease-in-out" data-aos-anchor-placement="top-bottom" data-aos-delay="300">
-                    <img src="/storage/blank.jpg" data-src="/storage/phone.png" alt="" class="lazy test-image device-image img3">
-                    <img src="/storage/blank.jpg" data-src="/storage/clickb4it_mobile.jpg" alt="" class="lazy test-image img4">
+                    <img src="/storage/phone.png" alt="" class="lazy test-image device-image img3">
+                    <img src="/storage/clickb4it_mobile.jpg" alt="" class="lazy test-image img4">
                 </div>
             </a>
         </div>
         <div class="web-tomstech">
             <a href="/tomstech" aria-label="TomsTech blog">
                 <div class="wireframe-container" data-aos="fade-right" data-aos-duration="1400" data-aos-easing="ease-in-out" data-aos-anchor-placement="top-bottom" data-aos-delay="300">
-                    <img src="/storage/blank.jpg" data-src="/storage/laptop.png" alt="" class="lazy test-image device-image img1">
-                    <img src="/storage/blank.jpg" data-src="/storage/TomsTech_fs.jpg" alt="" class="lazy test-image img2">
+                    <img src="/storage/laptop.png" alt="" class="lazy test-image device-image img1">
+                    <img src="/storage/TomsTech_fs.jpg" alt="" class="lazy test-image img2">
                 </div>
                 <div class="wireframe-container2" data-aos="fade-left" data-aos-duration="1400" data-aos-easing="ease-in-out" data-aos-anchor-placement="top-bottom" data-aos-delay="300">
-                    <img src="/storage/blank.jpg" data-src="/storage/phone.png" alt="" class="lazy test-image device-image img3">
-                    <img src="/storage/blank.jpg" data-src="/storage/TomsTech_mobile.png" alt="" class="lazy test-image img4">
+                    <img src="/storage/phone.png" alt="" class="lazy test-image device-image img3">
+                    <img src="/storage/TomsTech_mobile.png" alt="" class="lazy test-image img4">
                 </div>
             </a>
         </div>
