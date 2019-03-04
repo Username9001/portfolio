7572c6c22b0f647ca44e69a007bbc5b4fb301af0
Username9001 <erik.wubbels@gmail.com>
2019-03-04 03:04:10 +0100

    script fixes

--- resources/js/app.js
+++ resources/js/app.js
@@ -26,16 +26,18 @@ $(window).on('mousemove touchmove', function (e) {
 });
 
 // Typewriter
-var writer = document.getElementById('welcome-message');
-var typewriter = new Typewriter(writer, {
-    loop: true,
-    delay: 40
-});
-typewriter.typeString('I am Erik Wubbels, user experience designer and web developer.')
-    .pauseFor(400)
-    .typeString(' Scroll down to view some of my projects. ')
-    .pauseFor(400)
-    .typeString('<a href="/about">Or click here to read more about me.</a>')
-    .pauseFor(60000)
-    .start();
-
+if(document.getElementById('welcome-message')) {
+    var writer = document.getElementById('welcome-message');
+    var typewriter = new Typewriter(writer, {
+        loop: true,
+        delay: 40
+    });
+    typewriter
+        .typeString('I am Erik Wubbels, user experience designer and web developer.')
+        .pauseFor(400)
+        .typeString(' Scroll down to view some of my projects. ')
+        .pauseFor(400)
+        .typeString('<a href="/about">Or click here to read more about me.</a>')
+        .pauseFor(60000)
+        .start();
+};
